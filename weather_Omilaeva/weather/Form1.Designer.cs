0bc1512089d06c37b4fcfe49c33905fa21aea48d
yulyaomil <72448008+yulyaomil@users.noreply.github.com>
2020-12-07 16:16:40 +0300

    Add files via upload

---
+++ weather_Omilaeva/weather/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lCity = new System.Windows.Forms.Label();
+            this.lTemp = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(153, 211);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(488, 122);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(401, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Город";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(379, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Температура";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lCity
+            // 
+            this.lCity.AutoSize = true;
+            this.lCity.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lCity.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.lCity.Location = new System.Drawing.Point(403, 97);
+            this.lCity.Name = "lCity";
+            this.lCity.Size = new System.Drawing.Size(52, 23);
+            this.lCity.TabIndex = 3;
+            this.lCity.Text = "label3";
+            // 
+            // lTemp
+            // 
+            this.lTemp.AutoSize = true;
+            this.lTemp.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lTemp.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lTemp.Location = new System.Drawing.Point(403, 168);
+            this.lTemp.Name = "lTemp";
+            this.lTemp.Size = new System.Drawing.Size(52, 23);
+            this.lTemp.TabIndex = 4;
+            this.lTemp.Text = "label4";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bodoni MT Condensed", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(356, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Юлия Омилаева";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lTemp);
+            this.Controls.Add(this.lCity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lCity;
+        private System.Windows.Forms.Label lTemp;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ weather_Omilaeva/weather/Form1.cs
@@ -0,0 +1,40 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+using System.IO;
+
+namespace weather
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Donetsk&appid=1eaa4e8c2e0d05fef8140b2d11fbf888&units=metric";
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            StreamReader reader = new StreamReader(res.GetResponseStream());
+            string response = reader.ReadToEnd();
+            richTextBox1.Text = response;
+            WeatherResponse wr = JsonConvert.DeserializeObject<WeatherResponse>(response);
+            lCity.Text = wr.Name;
+            lTemp.Text = wr.Main.temp.ToString();
+        }
+    }
+}
